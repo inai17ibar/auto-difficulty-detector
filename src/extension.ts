330715347c42b9abfb20ba09b842d5439e95bbd0
Soichiro Inatani <inai17ibar@gmail.com>
2023-11-19 22:59:16 +0900

    変更内容は正しく取れていないが、コマンドパレットで拡張機能のアクティベートはできるよ。

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,76 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+function saveEditHistory(context: vscode.ExtensionContext, document: vscode.TextDocument, contentChanges: readonly vscode.TextDocumentContentChangeEvent[]) {
+	console.log("ファイル変更検出:", document.uri.toString());
+	
+	// ファイルの変更を記録する
+	const historyPath = path.join(context.extensionPath, 'editHistory.json');
+    let history = [];
+
+    if (fs.existsSync(historyPath)) {
+        history = JSON.parse(fs.readFileSync(historyPath, 'utf8'));
+    }
+
+    const editEntry = {
+        documentUri: document.uri.toString(),
+        timestamp: new Date().toISOString(), // 現在の時刻をISOフォーマットで記録
+        changes: contentChanges.map(change => ({
+            range: change.range,
+            text: change.text
+        }))
+    };
+
+	// 編集内容のログ
+    console.log("編集内容:", editEntry);
+
+	try {
+        history.push(editEntry);
+        fs.writeFileSync(historyPath, JSON.stringify(history, null, 2));
+    } catch (error) {
+        // エラーログ
+        console.error("履歴保存エラー:", error);
+    }
+}
+
 export function activate(context: vscode.ExtensionContext) {
+    // vscode.workspace.onDidChangeTextDocument(event => {
+    //     let activeEditor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "auto-difficulty-detector" is now active!');
+    //     // アクティブなエディタとイベントドキュメントの情報をログに出力
+    //     console.log("アクティブなエディタ:", activeEditor?.document.uri.toString());
+    //     console.log("イベントドキュメント:", event.document.uri.toString());
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('auto-difficulty-detector.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from auto-difficulty-detector!');
-	});
+    //     if (activeEditor && event.document === activeEditor.document) {
+    //         // 変更内容をログに出力
+    //         event.contentChanges.forEach(change => {
+    //             //console.log("変更されたテキスト:", change.text);
+    //             //console.log("変更範囲:", change.range);
+    //         });
+    //     }
+    // }, null, context.subscriptions);
 
-	context.subscriptions.push(disposable);
+    // // アクティブなエディタが変わったときのイベントリスナー
+    // vscode.window.onDidChangeActiveTextEditor(editor => {
+    //     console.log("アクティブなエディタが変更されました:", editor?.document.uri.toString());
+    // }, null, context.subscriptions);
+
+    // 拡張機能がアクティブになったことをログに記録
+    console.log("拡張機能がアクティブになりました");
+
+    let changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(event => {
+        saveEditHistory(context, event.document, event.contentChanges);
+    });
+
+    context.subscriptions.push(changeDocumentSubscription);
+
+    let disposable = vscode.commands.registerCommand('auto-difficulty-detector.activateExtension', () => {
+        // 拡張機能がアクティベートされたときのロジック
+        console.log('Auto Difficulty Detector is now active!');
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
